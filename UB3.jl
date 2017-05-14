98715568fab58e0f67bf0b54ab758fe9f40a5e53
HiOcPe <philip.jurytko@gmail.com>
2017-05-14 17:15:32 +0200

    Ableitung hinzugefügt

--- UB3.jl
+++ UB3.jl
@@ -6,3 +6,10 @@ end
 
 println("f(x) = sin(x)-0.3+0.5*x")
 println("f(0.2) = $(f(0.2))")
+
+#Erstelle Newton-Verfahren
+function f_prime(x)
+  return cos(x)+0.5
+end
+
+println("f'(x) = cos(x)+0.5")
